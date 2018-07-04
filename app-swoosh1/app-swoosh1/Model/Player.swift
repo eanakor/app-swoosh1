d8d8223c7f98cd16c872cff3ec0e90ffcff091f7
Macbook Pro <macbookpro@eches-macbook-pro.local>
2018-07-04 09:10:52 +0100

    IBActions, handling events and data models

---
+++ app-swoosh1/app-swoosh1/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh1
+//
+//  Created by Eche's Macbook  on 04/07/2018.
+//  Copyright © 2018 Eche's Macbook . All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
